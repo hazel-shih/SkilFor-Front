5cc3fef52b5544836554d2fb86f6bc34a0a10098
zoeliu <zoe.liu@gmail.com>
2021-11-18 15:45:02 +0800

    [style] homepage 切版完成

--- skilfor/src/pages/HomePage/HomePage.js
+++ skilfor/src/pages/HomePage/HomePage.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import banner from "../../img/banner.jpg";
-import beATeacher from "../../img/banner_teacher.jpg";
+import teacherStep from "../../img/teacher_step.jpg";
+import firstStep from "../../img/first_step.jpg";
+import secondStep from "../../img/second_step.jpg";
+import thirdStep from "../../img/third_step.jpg";
 import { MEDIA_QUERY_SM } from "../../components/constants/breakpoints";
 const Container = styled.div``;
 
@@ -40,12 +43,85 @@ const Banner = styled.section`
   }
 `;
 
-const StudentStep = styled.div``;
+const StudentStep = styled.div`
+  padding: 30px;
+`;
+
+const Title = styled.h1`
+  padding: 20px;
+  color: ${(props) => props.theme.colors.grey_dark};
+`;
+
+const StepDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  margin: 0 auto;
+  ${MEDIA_QUERY_SM} {
+    min-width: 768px;
+    width: 100%;
+  }
+`;
+
+const Img = styled.img`
+  display: block;
+  width: 60%;
+  height: 400px;
+  margin: 0 auto;
+  box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.1);
+`;
+
+const StepContent = styled.div`
+  padding: 0 25px;
+  width: 50%;
+  height: 400px;
+  color: ${(props) => props.theme.colors.green_dark};
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  font-weight: bold;
+  position: relative;
+`;
+
+const StepTitle = styled.h2``;
+
+const StepDescription = styled.p`
+  width: 100%;
+  margin: 12px 0px;
+  font-size: 1rem;
+`;
+
 const TeacherStep = styled(Banner)`
-  background: url(${beATeacher}) no-repeat center/cover;
+  background: url(${teacherStep}) no-repeat center/cover;
   & > p {
     transform: translate(-50%, -130%);
   }
+`;
+
+const BtnDiv = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Btn = styled.button`
+  border-radius: 40px;
+  border: none;
+  cursor: pointer;
+  background-color: ${(props) => props.theme.colors.orange};
+  color: white;
+  font-weight: bold;
+  padding: 20px;
+  margin: 24px;
+  min-width: 150px;
+  z-index: 3;
+  box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.1);
+`;
+
+const FindATeacherBtn = styled(Btn)`
+  position: absolute;
+  right: 0;
+  bottom: 0;
 `;
 
 function HomePage() {
@@ -61,12 +137,47 @@ function HomePage() {
           <br />
         </p>
       </Banner>
-      <StudentStep></StudentStep>
-      <TeacherStep src={beATeacher}>
+      <StudentStep>
+        <Title>上課三步驟</Title>
+        <StepDiv>
+          <Img src={firstStep}></Img>
+          <StepContent>
+            <StepTitle>Step 1：根據學習需求找老師</StepTitle>
+            <StepDescription>
+              完成帳號註冊後，進入「找老師」頁面選擇你要學習的領域，將會立即出現該領域的所有老師。
+            </StepDescription>
+          </StepContent>
+        </StepDiv>
+        <StepDiv>
+          <StepContent>
+            <StepTitle>Step 2：預約與購買課程</StepTitle>
+            <StepDescription>
+              挑好老師後，可點選老師的行事曆選擇可上課時間。點選後，可到「購物車」中查看，確認資訊無誤，按下送出。出現「購買成功」即代表成功買好老師的時間了。
+            </StepDescription>
+          </StepContent>
+          <Img src={secondStep}></Img>
+        </StepDiv>
+        <StepDiv>
+          <Img src={thirdStep}></Img>
+          <StepContent>
+            <StepTitle>Step 3：開始上課</StepTitle>
+            <StepDescription>
+              上課前會收到線上課程會議室連結，到了上課時間點選連結進入會議室，就可以開始享受與老師的上課啦！
+            </StepDescription>
+            <BtnDiv>
+              <FindATeacherBtn>開始找老師</FindATeacherBtn>
+            </BtnDiv>
+          </StepContent>
+        </StepDiv>
+      </StudentStep>
+      <TeacherStep src={teacherStep}>
         <p>
           上架你的才華，將熱情與技能分享給全世界
           <br />
         </p>
+        <BtnDiv>
+          <Btn>成為老師</Btn>
+        </BtnDiv>
       </TeacherStep>
     </Container>
   );
