ce8c09d511d9ff53d396ec7b270fe9bc2e7662e3
hazel-shih <hazelshih17@gmail.com>
2021-12-07 16:49:02 +0800

    [feat] 課程前台：新增 API 假資料

--- skilfor/src/pages/FrontCoursePage/FrontCoursePage.js
+++ skilfor/src/pages/FrontCoursePage/FrontCoursePage.js
@@ -1,16 +1,15 @@
+import { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 import { useParams } from "react-router";
 import { AvatarContainer } from "../../components/Avatar/Avatar";
 import FrontCourseCalendar from "./components/FrontCourseCalendar";
-import teacher from "../../img/teacher.jpeg";
-import student1 from "../../img/student1.png";
-import student2 from "../../img/student2.jpeg";
-import student3 from "../../img/student3.jpeg";
 import CommentCard from "./components/CommentCard";
 import {
   MEDIA_QUERY_MD,
   MEDIA_QUERY_SM,
 } from "../../components/constants/breakpoints";
+import { TEACHER_INFOS, COURSE_INFOS, COMMENTS } from "./constants";
+import { sleep } from "../../utils";
 
 const TeacherProfileWrapper = styled.section`
   padding: 180px 200px 232px 200px;
@@ -81,59 +80,74 @@ const SectionIntro = styled(ItemContent)`
   font-size: 1.1rem;
 `;
 const CommentsContainer = styled(ColumnContainer)``;
+const NoStatusText = styled.p`
+  color: ${(props) => props.theme.colors.grey_dark};
+  font-size: 1.2rem;
+`;
 
 function FrontCoursePage() {
+  window.scroll(0, 0);
   const { courseId } = useParams();
+  const [teacherInfos, setTeacherInfos] = useState(null);
+  const [courseInfos, setCourseInfos] = useState(null);
+  const [comments, setComments] = useState(null);
+
+  const FetchData = useCallback(async () => {
+    await sleep(500);
+    setTeacherInfos(TEACHER_INFOS);
+    setCourseInfos(COURSE_INFOS);
+    setComments(COMMENTS);
+  }, []);
+  useEffect(() => {
+    FetchData();
+  });
+
   return (
     <TeacherProfileWrapper>
       <TeacherInfosContainer>
         <TeacherAvatarContainer>
-          <TeacherAvatar imgSrc={teacher} name="Kelly" />
-          <AvatarName>Kelly</AvatarName>
+          {teacherInfos && (
+            <>
+              <TeacherAvatar imgSrc={teacherInfos.avatar} />
+              <AvatarName>{teacherInfos.username}</AvatarName>
+            </>
+          )}
         </TeacherAvatarContainer>
+
         <CourseInfosContainer>
           <ItemContainer>
             <ItemTitle>領域</ItemTitle>
-            <ItemContent>程式</ItemContent>
+            {courseInfos && <ItemContent>{courseInfos.category}</ItemContent>}
           </ItemContainer>
           <ItemContainer>
             <ItemTitle>課程名稱</ItemTitle>
-            <ItemContent>一起來學習超潮的 Ruby 吧！</ItemContent>
+            {courseInfos && <ItemContent>{courseInfos.courseName}</ItemContent>}
           </ItemContainer>
           <ItemContainer>
             <ItemTitle>單堂價格</ItemTitle>
-            <ItemContent>1000</ItemContent>
+            {courseInfos && <ItemContent>{courseInfos.price}</ItemContent>}
           </ItemContainer>
         </CourseInfosContainer>
       </TeacherInfosContainer>
       <SectionTitle>課程時間</SectionTitle>
       <FrontCourseCalendar courseId={courseId} />
       <SectionTitle>課程介紹</SectionTitle>
-      <SectionIntro>
-        經過幾年的發展，Spring Boot
-        的功能已經非常成熟，並且在近幾年軟體業盛行微服務（microservice）的設計模式下，也帶動越來越多企業選擇使用
-        Spring Boot 作為主流的開發工具。Spring Boot
-        之所以能夠成為目前業界最流行的開發工具，原因就在於 Spring Boot 憑借著
-        簡化 Spring 開發 以及 快速整合主流框架
-        的優點，讓工程師們可以更專注的在解決問題上，進而提升了前期開發和後續部署的效率。
-      </SectionIntro>
+      {courseInfos && (
+        <SectionIntro>{courseInfos.courseDescription}</SectionIntro>
+      )}
       <SectionTitle>課程評價</SectionTitle>
       <CommentsContainer>
-        <CommentCard
-          imgSrc={student1}
-          name="小豪"
-          content="課程將SQL的語法結構邏輯以及重要語法的功能與使用觀念，以非常循序漸進的主題節奏來教學，讓我能逐步由淺入深地建構對SQL重點觀念的理解，原本自己對於SQL有些粗淺的認識、但對於實際運用時的語法運作邏輯並不是那麼有把握，經過課程系統性的教學、以及每章節課後練習題立即的複習、演練、體驗該章節語法的運作邏輯，讓我釐清許多過去常常不知其所以然的語法邏輯卡關，實際演練練習題不僅能從做中學也讓人很有成就感，有動力及信心繼續深入學習。"
-        />
-        <CommentCard
-          imgSrc={student2}
-          name="小葵"
-          content="課程將SQL的語法結構邏輯以及重要語法的功能與使用觀念，以非常循序漸進的主題節奏來教學，讓我能逐步由淺入深地建構對SQL重點觀念的理解，原本自己對於SQL有些粗淺的認識、但對於實際運用時的語法運作邏輯並不是那麼有把握，經過課程系統性的教學、以及每章節課後練習題立即的複習、演練、體驗該章節語法的運作邏輯，讓我釐清許多過去常常不知其所以然的語法邏輯卡關，實際演練練習題不僅能從做中學也讓人很有成就感，有動力及信心繼續深入學習。"
-        />
-        <CommentCard
-          imgSrc={student3}
-          name="小花"
-          content="課程將SQL的語法結構邏輯以及重要語法的功能與使用觀念，以非常循序漸進的主題節奏來教學，讓我能逐步由淺入深地建構對SQL重點觀念的理解，原本自己對於SQL有些粗淺的認識、但對於實際運用時的語法運作邏輯並不是那麼有把握，經過課程系統性的教學、以及每章節課後練習題立即的複習、演練、體驗該章節語法的運作邏輯，讓我釐清許多過去常常不知其所以然的語法邏輯卡關，實際演練練習題不僅能從做中學也讓人很有成就感，有動力及信心繼續深入學習。"
-        />
+        {comments && comments.length !== 0 ? (
+          comments.map((comment) => (
+            <CommentCard
+              imgSrc={comment.imgSrc}
+              name={comment.username}
+              content={comment.content}
+            />
+          ))
+        ) : (
+          <NoStatusText>目前沒有任何評論</NoStatusText>
+        )}
       </CommentsContainer>
     </TeacherProfileWrapper>
   );
--- skilfor/src/pages/FrontCoursePage/constants.js
+++ skilfor/src/pages/FrontCoursePage/constants.js
@@ -1,3 +1,23 @@
+import student1 from "../../img/student1.png";
+import student2 from "../../img/student2.jpeg";
+import student3 from "../../img/student3.jpeg";
+import teacher from "../../img/teacher.jpeg";
+
+export const TEACHER_INFOS = {
+  username: "Kelly",
+  avatar: teacher,
+};
+
+export const COURSE_INFOS = {
+  category: "程式",
+  courseName: "一起來學習超潮的 Ruby 吧！",
+  courseDescription:
+    "經過幾年的發展，Spring Boot 的功能已經非常成熟，並且在近幾年軟體業盛行微服務（microservice）的設計模式下，也帶動越來越多企業選擇使用 Spring Boot 作為主流的開發工具。Spring Boot 之所以能夠成為目前業界最流行的開發工具，原因就在於 Spring Boot 憑借著 簡化 Spring 開發 以及 快速整合主流框架 的優點，讓工程師們可以更專注的在解決問題上，進而提升了前期開發和後續部署的效率。",
+  price: 1000,
+  published: true,
+  audit: "success",
+};
+
 export const COURSE_EVENT_LIST = [
   {
     title: "一起來學習超潮的 Ruby 吧！",
@@ -58,5 +78,26 @@ export const COURSE_EVENT_LIST = [
       timePeriod: "22:30 ~ 23:00",
     },
     id: "Plisyjyva2EPNFXJbTUUs",
+  },
+];
+
+export const COMMENTS = [
+  {
+    imgSrc: student1,
+    username: "小豪",
+    content:
+      "課程將SQL的語法結構邏輯以及重要語法的功能與使用觀念，以非常循序漸進的主題節奏來教學，讓我能逐步由淺入深地建構對SQL重點觀念的理解，原本自己對於SQL有些粗淺的認識、但對於實際運用時的語法運作邏輯並不是那麼有把握，經過課程系統性的教學、以及每章節課後練習題立即的複習、演練、體驗該章節語法的運作邏輯，讓我釐清許多過去常常不知其所以然的語法邏輯卡關，實際演練練習題不僅能從做中學也讓人很有成就感，有動力及信心繼續深入學習。",
+  },
+  {
+    imgSrc: student2,
+    username: "小葵",
+    content:
+      "課程將SQL的語法結構邏輯以及重要語法的功能與使用觀念，以非常循序漸進的主題節奏來教學，讓我能逐步由淺入深地建構對SQL重點觀念的理解，原本自己對於SQL有些粗淺的認識、但對於實際運用時的語法運作邏輯並不是那麼有把握，經過課程系統性的教學、以及每章節課後練習題立即的複習、演練、體驗該章節語法的運作邏輯，讓我釐清許多過去常常不知其所以然的語法邏輯卡關，實際演練練習題不僅能從做中學也讓人很有成就感，有動力及信心繼續深入學習。",
+  },
+  {
+    imgSrc: student3,
+    username: "小花",
+    content:
+      "課程將SQL的語法結構邏輯以及重要語法的功能與使用觀念，以非常循序漸進的主題節奏來教學，讓我能逐步由淺入深地建構對SQL重點觀念的理解，原本自己對於SQL有些粗淺的認識、但對於實際運用時的語法運作邏輯並不是那麼有把握，經過課程系統性的教學、以及每章節課後練習題立即的複習、演練、體驗該章節語法的運作邏輯，讓我釐清許多過去常常不知其所以然的語法邏輯卡關，實際演練練習題不僅能從做中學也讓人很有成就感，有動力及信心繼續深入學習。",
   },
 ];
