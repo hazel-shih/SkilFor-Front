8659a882c1f852ea75c9143c3ac0c53bc2505dbb
hazel-shih <hazelshih17@gmail.com>
2021-12-15 16:42:46 +0800

    [feat] 新增學生行事曆頁面

--- skilfor/src/pages/StudentCalendarPage/StudentCalendarPage.js
+++ skilfor/src/pages/StudentCalendarPage/StudentCalendarPage.js
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+import PageTitle from "../../components/PageTitle";
+import StudentManageCalendar from "../../components/Calendar/StudentManageCalendar";
+import { TeacherCalendarWrapper } from "../TeacherCalendarPage/TeacherCalendarPage";
+
+const StufentCalendarWrapper = styled(TeacherCalendarWrapper)``;
+
 function StudentCalendarPage() {
-  return <div></div>;
+  window.scroll(0, 0);
+  return (
+    <StufentCalendarWrapper>
+      <PageTitle>行事曆</PageTitle>
+      <StudentManageCalendar />
+    </StufentCalendarWrapper>
+  );
 }
 
 export default StudentCalendarPage;
