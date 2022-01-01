a3677fc5df46ea6df0397a82718dc1be4993c21e
hazel-shih <hazelshih17@gmail.com>
2022-01-01 14:27:00 +0800

    [fix] 點數儲值頁面改寫產生訂單時間函式以修復格式不對的問題

--- src/pages/PointPage/utils.js
+++ src/pages/PointPage/utils.js
@@ -37,7 +37,14 @@ export const createMerchantTradeNo = () => {
 };
 
 export const getCurrentTime = () => {
-  let dateDataArr = new Date().toLocaleString().split(" ");
-  let timeDataArr = new Date().toTimeString().split(" ");
-  return dateDataArr[0] + " " + timeDataArr[0];
+  var dt = new Date();
+  return `${dt.getFullYear().toString().padStart(4, "0")}/${(dt.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}/${dt.getDate().toString().padStart(2, "0")} ${dt
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${dt.getMinutes().toString().padStart(2, "0")}:${dt
+    .getSeconds()
+    .toString()
+    .padStart(2, "0")}`;
 };
